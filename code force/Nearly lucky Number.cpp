4c30d5482504d4539db6b89fb0c768c5a4a03bfc
MD-ALAMIN-HOSSEN <99692491+MD-ALAMIN-HOSSEN@users.noreply.github.com>
2022-05-15 20:38:17 +0600

    Update Nearly lucky Number.cpp

--- code force/Nearly lucky Number.cpp
+++ code force/Nearly lucky Number.cpp
@@ -1,19 +1,19 @@
-#include<iostream>
+#include<bits/stdc++.h>
 using namespace std;
 int main(){
     ios_base::sync_with_stdio(false);
     cin.tie(nullptr);
     int a =0;
-    int x,r,i;
-    cin>>x;
-    i=x;
+    int r;
+    long long i;
+    cin>>i;
     while (i!=0){
         r=i%10;
         i/=10;      
-        if (r!=4 && r!=7){
-           a++;
+        if (r==4 || r==7){
+           a++;        
         };      
-    }
-    (a<=0) ? cout<<"YES" : cout<<"NOT";    
+    }  
+    (a==4||a==7) ? cout<<"YES" : cout<<"NO";    
     return EXIT_SUCCESS;
 }
